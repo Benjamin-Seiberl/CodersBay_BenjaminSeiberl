5c7db9cfbd6d38f2dafc5318b752235c83c1b323
Benjamin-Seiberl <seiberl.benq@gmail.com>
2022-08-16 15:23:10 +0200

    Added A_2_3_NotenÜbersetzen.java

---
+++ src/A_2_3_NotenÜbersetzen.java
@@ -0,0 +1,6 @@
+public class A_2_3_NotenÜbersetzen {
+    public static void main(String[] args) {
+        int mark = UserInput.getInt(1,5,"Gib eine Zahl zwischen 1 und 5 ein!");
+        
+    }
+}
