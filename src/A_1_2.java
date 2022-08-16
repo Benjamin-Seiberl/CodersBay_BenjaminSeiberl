c8a8642e385f78f349608d03f76f9b39d20dba51
Benjamin-Seiberl <seiberl.benq@gmail.com>
2022-08-16 14:41:46 +0200

    Added A_1_2.java

---
+++ src/A_1_2.java
@@ -0,0 +1,21 @@
+public class A_1_2 {
+    public static void main(String[] args) {
+        int num1 = 52;
+        int num2 = 12;
+
+        System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1/num2));
+
+        double num3 = 52;
+        double num4 = 12;
+
+        System.out.println(num3 + " + " + num4 + " = " + (num3+num4));
+        System.out.println(num3 + " - " + num4 + " = " + (num3-num4));
+        System.out.println(num3 + " * " + num4 + " = " + (num3*num4));
+        System.out.println(num3 + " / " + num4 + " = " + (num3/num4));
+
+    }
+
+}
