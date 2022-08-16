e0d41ac3de02d285ac6505509a7371a12916db46
Benjamin-Seiberl <seiberl.benq@gmail.com>
2022-08-16 14:57:29 +0200

    Finished A_1_3.java

--- src/A_1_3.java
+++ src/A_1_3.java
@@ -2,5 +2,15 @@ public class A_1_3 {
     public static void main(String[] args) {
         String name = "Hello World";
         System.out.println(name + " is " + name.length() + " chars long!");
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        name = "Hello Codersbay";
+        System.out.println(name);
+        
+
+        System.out.println();
+        for(int i = 0; i < 15; i++){
+            System.out.println((i+1) + ": " + name);
+        }
     }
 }
