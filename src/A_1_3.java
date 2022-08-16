bb8e71bf2e10f79d44b062b63beaf45592112a80
Benjamin-Seiberl <seiberl.benq@gmail.com>
2022-08-16 14:51:52 +0200

    Added A_1_3.java

---
+++ src/A_1_3.java
@@ -0,0 +1,6 @@
+public class A_1_3 {
+    public static void main(String[] args) {
+        String name = "Hello World";
+        System.out.println(name + " is " + name.length() + " chars long!");
+    }
+}
