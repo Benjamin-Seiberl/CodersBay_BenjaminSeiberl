16398a3568364c6af711ec134404b338480f9733
Benjamin-Seiberl <seiberl.benq@gmail.com>
2022-08-16 13:59:33 +0200

    Finished A_1_1.java

--- src/A_1_1.java
+++ src/A_1_1.java
@@ -1,3 +1,13 @@
+import java.util.Date;
+
 public class A_1_1 {
-    
+    int age = 23;
+    String firstname = "Benjamin";
+    String lastname = "Seiberl";
+    char gender = 'm';
+    int year = 1999;
+    int month = 7;
+    int day = 22;
+    double averageMarks = 1.77;
+    boolean married = false;
 }
