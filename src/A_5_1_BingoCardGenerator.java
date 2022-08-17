b804187cd8fa15e309a2962b52c192bd2fe9b5a1
Benjamin-Seiberl <seiberl.benq@gmail.com>
2022-08-17 11:10:33 +0200

    Finished A_5_1_BingoCardGenerator.java

--- src/A_5_1_BingoCardGenerator.java
+++ src/A_5_1_BingoCardGenerator.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class A_5_1_BingoCardGenerator {
     public static void main(String[] args) {
+        int[][] bingoBoard = new int[5][5];
+        Random r = new Random();
+        int max = 15;
+        int min = 1;
+        for(int i = 0; i < bingoBoard.length; i++){
+            System.out.print("|");
+            for(int j = 0; j < bingoBoard[i].length; j++) {
+                if (i != 2 || j != 2) {
+                    bingoBoard[i][j] = r.nextInt(max + 1 - min) + min;
+                    System.out.printf(" %3d |", bingoBoard[i][j]);
+                }else{
+                    System.out.print("     |");
+                }
+            }
+            System.out.println();
+            max += 15;
+            min += 15;
+        }
 
     }
 }
