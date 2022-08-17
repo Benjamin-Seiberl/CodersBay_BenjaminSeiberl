e16a10516ecad913c0f714bc3eda38488dddd453
Benjamin-Seiberl <seiberl.benq@gmail.com>
2022-08-17 09:26:38 +0200

    Finished A_3_1_FizzBuzz.java

--- src/A_3_1_FizzBuzz.java
+++ src/A_3_1_FizzBuzz.java
@@ -1,5 +1,18 @@
 public class A_3_1_FizzBuzz {
     public static void main(String[] args) {
-        
+        for(int i = 0; i < 100; i++){
+            if((i % 3 == 0 && i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i + "!");
+            }
+        }
     }
 }
