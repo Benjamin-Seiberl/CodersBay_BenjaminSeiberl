6a0fbede7288a12d1fc0fcaa0fe6aa496ea1306f
Benjamin-Seiberl <seiberl.benq@gmail.com>
2022-08-17 09:34:01 +0200

    Finished A_3_2_DasKleineEinmalEins.java

--- src/A_3_2_DasKleineEinmalEins.java
+++ src/A_3_2_DasKleineEinmalEins.java
@@ -1,5 +1,10 @@
 public class A_3_2_DasKleineEinmalEins {
     public static void main(String[] args) {
-
+        for(int i = 0; i <= 10; i++){
+            for(int j = 0; j <= 10; j++){
+                System.out.printf("%2d * %2d  = %3d",j,i,(j*i));
+                System.out.println();
+            }
+        }
     }
 }
